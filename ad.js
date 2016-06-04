84b1143a8ecbedba3661bb0925b3ca9f05a5cc3e
Mike Wang <mikewang000000@gmail.com>
2016-06-04 22:55:22 +0800

    简化代码，并增加详细注释，方便小白修改。

--- ad.js
+++ ad.js
@@ -12,47 +12,128 @@
 
 */
 
-var BLACK_HOLE = "PROXY example.com:80";
-var DIRECT = "DIRECT";
+function FindProxyForURL(url, host){
 
-function n(h, r, m) { return isInNet(h, r, m); }
-function s(u, r) { return shExpMatch(u, r); }
-function d(h, r){ return dnsDomainIs(h, r); }
+    //符合以下规则的域名或IP地址将通过下面的代理服务器访问。由于example.com:80【不是代理服务器】，故符合以下规则的域名或IP地址将【无法访问】，可实现屏蔽广告效果。
+    var BLACK_HOLE = "PROXY example.com:80";
 
-function FindProxyForURL(url, host)
-{
-    var u = url.toLowerCase();
-    var h = host.toLowerCase();
+    //不符合以下规则的域名或IP地址直连。
+    var DIRECT = "DIRECT";
 
-//ad.js
-if( d(h, "sc.cdce.cf") ){ return BLACK_HOLE; }
-//youku
-if( d(h, "ad.api.3g.youku.com") || d(h, "statis.api.3g.youku.com") || d(h, "atm.youku.com") || d(h, "stat.youku.com") ){ return BLACK_HOLE; }
-//sohu
-if( d(h, "agn.aty.sohu.com") || d(h, "mmg.aty.sohu.com") ){ return BLACK_HOLE; }
-//letv
-if( d(h, "n.mark.letv.com") || d(h, "ark.letv.com") || d(h, "irs01.com") ){ return BLACK_HOLE; }
-//baofeng
-if( d(h, "xs.houyi.baofeng.net") ){ return BLACK_HOLE; }
-//hunantv
-if( d(h, "da.hunantv.com" ) || d(h, "miaozhen.com" ) ){ return BLACK_HOLE; }
-//qq
-if( d(h, "lives.l.qq.com" ) || d(h, "vqq.admaster.com.cn") ) {return BLACK_HOLE; }
-//tudou
-if( d(h, "ad.api.3g.tudou.com") ){ return BLACK_HOLE; }
-//pptv
-if( d(h, "de.as.pptv.com" ) || d(h, "pp2.pptv.com" ) || d(h, "stat.pptv.com" ) || d(h, "afp.pplive.com" ) ){ return BLACK_HOLE; }
-//iqiyi
-if( d(h, "cm.passport.iqiyi.com") || d(h, "cupid.iqiyi.com") || d(h, "paopao.iqiyi.com") || d(h, "ckm.iqiyi.com") || n(h, "101.227.14.0", "255.255.255.0") ){ return BLACK_HOLE; }
-//google
-if( d(h, "2mdn.net" ) || d(h, "admob.com" ) || d(h, "doubleclick.com" ) || d(h, "doubleclick.net" ) || d(h, "googleadsserving.cn" ) || d(h, "googlecommerce.com" ) || d(h, "googletagmanager.com" ) || d(h, "ads.google.com" ) || d(h, "afd.l.google.com" ) || d(h, "pagead-tpc.l.google.com" ) || d(h, "pagead.google.com" ) || d(h, "pagead.l.google.com" ) || d(h, "partnerad.l.google.com" ) ){ return BLACK_HOLE; }
-//baidu
-if( d(h, "hm.baidu.com" ) || d(h, "eiv.baidu.com" ) || d(h, "pos.baidu.com" ) || d(h, "cpro.baidu.com" ) || d(h, "cpro.baidustatic.com" ) || d(h, "dup.baidustatic.com" ) ){ return BLACK_HOLE; }
-//taobao
-if( d(h, "tanx.com") || d(h, "alimama.com" ) ){ return BLACK_HOLE; }
-//360
-if( d(h, "lianmeng.360.cn") ){ return BLACK_HOLE; }
-//others
-if( d(h, "keydot.net" ) || d(h, "mengmob.com" ) || d(h, "zzzkxx.com" ) || d(h, "miaozhen.com" ) || d(h, "adsage.com" ) || d(h, "cocounion.com" ) || d(h, "admaster.com.cn" ) || d(h, "ipinyou.com" ) || d(h, "admin5.com" ) || d(h, "csbew.com" ) || d(h, "jusha.com" ) || d(h, "vsoon.com" ) || d(h, "soquair.com" ) || d(h, "guohead.com" ) || d(h, "live.chartboost.com" ) || d(h, "appodeal.com" ) || d(h, "fastapi.net" ) || d(h, "adsmogo.com" ) || d(h, "adsmogo.org" ) || d(h, "adsmogo.net" ) || d(h, "adsmogo.mobi" ) || d(h, "adlive.cn" ) || d(h, "sstc360.com" ) || d(h, "ad-brix.com" ) || d(h, "leadboltads.net" ) || d(h, "joyingmobi.com" ) ){ return BLACK_HOLE; }
+    var hosts = [
+//=========域名Start=========
+//<ad.js - test>
+"sc.cdce.cf",
+//<youku>
+"ad.api.3g.youku.com",
+"statis.api.3g.youku.com",
+"atm.youku.com",
+"stat.youku.com",
+//<sohu>
+"agn.aty.sohu.com",
+"mmg.aty.sohu.com",
+//<letv>
+"n.mark.letv.com",
+"ark.letv.com",
+"irs01.com",
+//<baofeng>
+"xs.houyi.baofeng.net",
+//<hunantv>
+"da.hunantv.com",
+"miaozhen.com",
+//<qq>
+"lives.l.qq.com",
+"vqq.admaster.com.cn",
+//<tudou>
+"ad.api.3g.tudou.com",
+//<pptv>
+"de.as.pptv.com",
+"pp2.pptv.com",
+"stat.pptv.com",
+"afp.pplive.com",
+//<iqiyi>
+"cm.passport.iqiyi.com",
+"cupid.iqiyi.com",
+"paopao.iqiyi.com",
+"ckm.iqiyi.com",
+//<google>
+"2mdn.net",
+"admob.com",
+"doubleclick.com",
+"doubleclick.net",
+"googleadsserving.cn",
+"googlecommerce.com",
+"googletagmanager.com",
+"ads.google.com",
+"afd.l.google.com",
+"pagead-tpc.l.google.com",
+"pagead.google.com",
+"pagead.l.google.com",
+"partnerad.l.google.com",
+//<baidu>
+"hm.baidu.com",
+"eiv.baidu.com",
+"pos.baidu.com",
+"cpro.baidu.com",
+"cpro.baidustatic.com",
+"dup.baidustatic.com",
+//<taobao>
+"tanx.com",
+"alimama.com",
+//<360>
+"lianmeng.360.cn",
+//<others>
+"keydot.net",
+"mengmob.com",
+"zzzkxx.com",
+"miaozhen.com",
+"adsage.com",
+"cocounion.com",
+"admaster.com.cn",
+"ipinyou.com",
+"admin5.com",
+"csbew.com",
+"jusha.com",
+"vsoon.com",
+"soquair.com",
+"guohead.com",
+"live.chartboost.com",
+"appodeal.com",
+"fastapi.net",
+"adsmogo.com",
+"adsmogo.org",
+"adsmogo.net",
+"adsmogo.mobi",
+"adlive.cn",
+"sstc360.com",
+"ad-brix.com",
+"leadboltads.net",
+"joyingmobi.com"
+//=========域名End=========
+//【在分界线上面可以追加域名，两边加上双引号，使用逗号分隔。】
+    ]
+    var ips = [
+//=========IP地址Start=========
+//<iqiyi>
+"101.227.14.80",
+"101.227.14.81",
+"101.227.14.82",
+"101.227.14.83",
+"101.227.14.84",
+"101.227.14.85",
+"101.227.14.86"
+//=========IP地址End=========
+//【在分界线上面可以追加IP地址，两边加上双引号，使用逗号分隔。】
+    ]
+    for (n in hosts){
+        if(dnsDomainIs(host, hosts[n])){
+            return BLACK_HOLE;
+        }
+    }
+    for (n in ips){
+        if(isInNet(host, ips[n], "225.225.225.225")){
+            return BLACK_HOLE;
+        }
+    }
     return DIRECT;
 }
