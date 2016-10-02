8236f4be798c1d91782a9cdd819194742d07dd49
Mike Wang <mikewang000000@gmail.com>
2016-10-02 17:53:33 +0800

    解决失效问题

--- ad.js
+++ ad.js
@@ -369,7 +369,7 @@ function FindProxyForURL(url, host){
     ]
 
     dnsResolve("sc.cdce.cf");
-    var IS_AD = "PROXY example.com:80";
+    var IS_AD = "PROXY example.com:443";
     switch (mode){
       case 0:
           IS_NOT_AD = "DIRECT";
